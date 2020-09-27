f1a49aacda1df92aea5910225f256fc24548b3eb
Luke <rohenaz@gmail.com>
2020-09-26 20:41:21 -0400

    crawler

--- main.go
+++ main.go
@@ -1,112 +1,21 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"context"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/rohenaz/go-bap"
-	"github.com/rohenaz/go-bmap"
-	"github.com/rohenaz/go-bob"
-	"github.com/tidwall/sjson"
-	"github.com/tonicpow/bap-planaria-go/database"
 	"github.com/tonicpow/bap-planaria-go/persist"
 	"github.com/tonicpow/bap-planaria-go/router"
 	"github.com/tonicpow/bap-planaria-go/state"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 // Constants
 var currentBlock = 590000
 var fromBlock = currentBlock
 var stateBlock = 0
-
-func crawl(query []byte, height int) {
-	client := http.Client{}
-	// Create a timestamped query by applying the "$gt" (greater then) operator with the height
-	njson, _ := sjson.Set(string(query), `q.find.blk\.i.$gt`, height)
-	bjson := []byte(njson)
-	req, err := http.NewRequest("POST", "https://bob.bitbus.network/block", bytes.NewBuffer(bjson))
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("token", os.Getenv("PLANARIA_TOKEN"))
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	conn, err := database.Connect(ctx)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Printf("Initializing from block %d\n", currentBlock)
-
-	defer conn.Disconnect(ctx)
-	reader := bufio.NewReader(resp.Body)
-	// Split NDJSON stream by line
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-
-		bobData := bob.New()
-		err = bobData.FromBytes(line)
-		if err != nil {
-			fmt.Println("Error: 1", err)
-			return
-		}
-
-		if int(bobData.Blk.I) > currentBlock {
-			currentBlock = int(bobData.Blk.I)
-		}
-		// Transform from BOB to BMAP
-		bmapData := bmap.New()
-		err = bmapData.FromBob(bobData)
-		if err != nil {
-			log.Println("Error", err)
-		}
-
-		bsonData := bson.M{
-			"tx":  bobData.Tx,
-			"in":  bobData.In,
-			"out": bobData.Out,
-			"blk": bobData.Blk,
-		}
-
-		if bmapData.AIP != nil {
-			bsonData["AIP"] = bmapData.AIP
-		}
-
-		if bmapData.BAP != nil {
-			bsonData["BAP"] = bmapData.BAP
-		}
-
-		if bmapData.MAP != nil {
-			bsonData["MAP"] = bmapData.MAP
-		}
-
-		collectionName := bmapData.BAP.Type
-		filter := bson.M{"tx.h": bson.M{"$eq": bmapData.Tx.H}}
-
-		// Write to DB
-		_, err = conn.UpsertOne(string(collectionName), filter, bsonData)
-
-	}
-
-	// Print tx line to stdout
-	if err != nil {
-		fmt.Println(err)
-	}
-}
 
 func main() {
 
@@ -127,7 +36,7 @@ func main() {
 	}`)
 
 	// crawl will mutate currentBlock as it crawls forward from the given block height
-	crawl(q, currentBlock)
+	crawler.crawl(q, currentBlock)
 
 	diff := time.Now().Sub(then).Seconds()
 	fmt.Printf("Bitbus sync complete in %fs\nBlock height: %d\nSync height: %d\n", diff, currentBlock, stateBlock)
@@ -153,6 +62,7 @@ func main() {
 	// update the state block clounter
 	stateBlock = currentBlock
 	state.SaveProgress(uint32(stateBlock))
+
 	time.Sleep(30 * time.Second)
 	main()
 }
